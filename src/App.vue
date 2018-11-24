893ad63e1fc0530f345e021e9bebe545126e0803
Sciuridae0603 <Sciuridae0603@gmail.com>
2018-11-25 01:11:38 +0800

    add dist

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,100 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <div class="ts container">
+            <div class="ts grid">
+                <div class="sixteen wide column">
+                    <div class="ts secondary menu">
+                        <a class="item">首頁</a>
+                        <div class="right menu">
+                            <div id="btns" class="ts stackable buttons"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="ts grid">
+                <div class="six wide column">
+                    <Country id="map" :stroke="stroke" :scale="scale" :width="width" :height="height" :lon="lon/100" :lat="lat/100" :fill="fill" :fit="fit"></Country>
+                </div>
+                <div class="ten wide column">
+                    <br>
+                    <div class="ts fluid large divided statistics">
+                        <div class="statistic">
+                            <div class="value" id="Agree">0</div>
+                            <div class="label">同意票數</div>
+                        </div>
+                        <div class="statistic">
+                            <div class="value" id="Disagree">0</div>
+                            <div class="label">不同意票數</div>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="ts fluid large divided statistics">
+                        <div class="statistic">
+                            <div class="value" id="Effective">0</div>
+                            <div class="label">有效票數</div>
+                        </div>
+                        <div class="statistic">
+                            <div class="value" id="Invalid">0</div>
+                            <div class="label">無效票數</div>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="ts fluid large divided statistics">
+                        <div class="statistic">
+                            <div class="value" id="Votes">0</div>
+                            <div class="label">投票數</div>
+                        </div>
+                        <div class="statistic">
+                            <div class="value" id="VotingRights">0</div>
+                            <div class="label">投票權人數</div>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="ts fluid large divided statistics">
+                        <div class="statistic">
+                            <div class="value" id="VoteRate">0</div>
+                            <div class="label">投票率(%)</div>
+                        </div>
+                        <div class="statistic">
+                            <div class="value" id="EffectiveVotingRights">0</div>
+                            <div class="label">有效同意票數對投票權人數百分比(%)</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import {
+        Country
+    } from "taiwan-vue-components";
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: "app",
+        components: {
+            Country
+        },
+        mounted: function() {
+            this.resizeHandler()
+            window.addEventListener('resize', this.resizeHandler)
+        },
+        methods: {
+            resizeHandler: function() {
+                this.width =  window.innerWidth - 30
+                this.height = window.innerHeight - 30
+            }
+        },
+        data: {
+            fit: true,
+            width: 800,
+            height: 600,
+            lon: 12135,
+            lat: 2364,
+            stroke: '#000000',
+            fill: '#F9F9F9',
+            scale: 7800
+        }
+    };
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
